8bbf3a8e692386538e47d1d2ce64cf009d30d1a3
NHEBotx <cocnammax@gmail.com>
2025-01-17 04:44:31 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/bash
+apt-get update -y
+apt-get upgrade -y
+apt-get install yarn 
+apt-get install unzip -y
+apt-get install zip -y
+apt-get install nodejs -y
+apt-get install bash -y
+apt-get install git -y
+apt-get install ffmpeg -y
+apt-get install imagemagick -y
+apt-get install mc
+unzip baileys.zip -d node_modules/@adiwajshing/
+npm i
+
+echo -e "\e[1;41m[ ⚠️ BACA ANJING GA MAKEK DOANG ⚠️ ]\e[0m \e[1;37mBiar Sclu fungsi, biar lu ga Nyalain scnya Yang gabisa\e[0m"
+echo -e "\e[1;42m[ 🔑 ]\e[0m \e[1;37mKalo error, Minimal Download Baileysnya Berbentuk ZIP, Baileys.zip Nya ada di repository @NHEbotx ATAU DOWNLOAD DI GROUP, LINK GROUP? [ https://chat.whatsapp.com/Bc0N1yYfNwI25RMDOrrwjg ], terus PINDAHIN / SALIN AJA KE FOLDER BOT\e[0m"
+echo -e "\e[1;43m[❗]\e[0m \e[1;37mKALO UDAH LU DOWNLOAD LU SALIN/ TEMPEL DI FOLDER BOT, JANGAN DIBUKA ZIPNYA [ 📌 ]. MASUK KE TERMUX LAGI LALU KETIK INI\e[0m"
+echo -e "\e[1;45m[ unzip baileys.zip -d node_modules/@adiwajshing/ ]\e[0m \e[1;37mSorry kalo ribet, soalnya lu gratisan MAKEK SC, MODULE SCNYA GW SIMPEN DI GROUP\e[0m"
+echo -e "\e[1;44mUNTUK MENJALANKAN SCRIPT [👍 npm start]\e[0m"
+echo -e "\e[1;46m[ ✅ ] UDAH MAKEK SC MINIMAL KLIK BINTANG DI GITHUB, DAN MINIMAL JANGAN HAPUS (tqto) TANPA MEREKA LU GABAKAL DAPET INI SC, YA ANJING\e[0m"
+
+
+#🔑 GANTI OWNER DI CONFIG.JS & OWNER.JSON & SRC/DATA/ROLE/CONTACT.JSON
+
+#📝 JANGAN ADA YANG KELEWATAN! KALO SALAH NANTI SATIR KE SC NYA, PADAHAL MASANGNYA AJA GA BENER☺️
+
+#/*
+ #✅ 𝘽𝘼𝘾𝘼, 𝙁𝘼𝙃𝘼𝙈𝙄, 𝘿𝘼𝙉 𝙇𝘼𝙆𝙐𝙄𝙉
+ #- Mau lu jual atau ngga, Urusanmu. tanggung jawabmu pas di Akhirat nanti. udh dikasih dan lu dapet scnya gratis malah dijual (ga amanah)
+# - ini makek base dari Yurin'S
+ #- Jangan ubah Tqto, tanpa mereka, sc ini ga akan pernah rilis & gabakalan kalian dapat
+ #⚠️ 𝐈𝐧𝐠𝐚𝐭 𝐒𝐞𝐛𝐚𝐠𝐢𝐚𝐧, 𝐒𝐜 𝐀𝐤𝐮 𝐄𝐧𝐜, 𝐌𝐚𝐮 𝐲𝐚𝐧𝐠 𝐍𝐨 𝐄𝐍𝐂? 𝐆𝐚𝐛𝐮𝐧𝐠 𝐤𝐞 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩 & 𝐆𝐫𝐨𝐮𝐩
+ #CHANNEL WA:
+ #https://whatsapp.com/channel/0029Vb2kT5pJ3jv5yIVMsx2q
+# GROUP WA:
+ #https://chat.whatsapp.com/Bc0N1yYfNwI25RMDOrrwjg
+       #* 👑 ©NHE-NHEBotx - 2025
+      # https://github.com/NHEBotx
